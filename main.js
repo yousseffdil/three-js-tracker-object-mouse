6ace0e934e17633c6bf94512ebe2d99607309444
yousseffdil <fdilyoussef@gmail.com>
2024-08-08 22:49:59 +0200

    remove comments

--- main.js
+++ main.js
@@ -11,40 +11,38 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 2;
 
 // Renderizador
-const renderer = new THREE.WebGLRenderer({ alpha: true }); // Hacer el fondo del canvas transparente
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true; // Habilitar sombras
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 // Cargar la textura
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load("../texture.jpg"); // Cambia esta ruta a la ubicación de tu textura
-
-// Geometría y material del cubo
+const texture = textureLoader.load("../texture.jpg");
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
-  // Cambiar a MeshStandardMaterial para sombras
   map: texture,
   side: THREE.DoubleSide,
 });
 const cube = new THREE.Mesh(geometry, material);
-cube.castShadow = true; // El cubo proyecta sombra
+cube.castShadow = true;
 cube.position.y = 0.1;
 scene.add(cube);
 
 // Añadir una fuente de luz
 const light = new THREE.PointLight(0xffffff, 1.5, 100);
 light.position.set(5, 5, 5);
-light.castShadow = true; // La luz puede proyectar sombras
+light.castShadow = true;
 scene.add(light);
 
 // Añadir un plano para recibir sombras
 const planeGeometry = new THREE.PlaneGeometry(10, 10);
-const planeMaterial = new THREE.ShadowMaterial({ opacity: 0.5 }); // Material para recibir sombra
+const planeMaterial = new THREE.ShadowMaterial({ opacity: 0.5 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
 plane.rotation.x = -Math.PI / 2;
 plane.position.y = -0.8;
-plane.receiveShadow = true; // El plano recibe sombras
+plane.receiveShadow = true;
 scene.add(plane);
 
 // Vector para la posición del ratón
@@ -64,22 +62,17 @@ const lerpFactor = 0.1;
 function animate() {
   requestAnimationFrame(animate);
 
-  // Convertir la posición del ratón a coordenadas del espacio 3D
   const vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
   vector.unproject(camera);
 
-  // Calcular la dirección desde la cámara hacia el vector del ratón
   const direction = vector.sub(camera.position).normalize();
 
-  // Crear un punto objetivo en la dirección del ratón
   const targetPosition = camera.position.clone().add(direction);
 
-  // Orientar la cara del cubo para que mire hacia el objetivo
   targetQuaternion.setFromRotationMatrix(
     new THREE.Matrix4().lookAt(cube.position, targetPosition, camera.up)
   );
 
-  // Interpolar la rotación del cubo
   cube.quaternion.slerp(targetQuaternion, lerpFactor);
 
   renderer.render(scene, camera);
